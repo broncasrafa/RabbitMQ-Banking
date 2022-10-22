ad72ef8177085e624f1ef9351f4423a6d81d5ca3
Rafael Francisco <broncasrafa@gmail.com>
2022-10-22 12:06:00 -0300

    Initial commit of Infrastructure Dependency Project IoC

---
+++ src/BankingRabbitMQ.Infrastructure.IoC/DependencyContainer.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using BankingRabbitMQ.Core.Domain.Bus;
+using BankingRabbitMQ.Infrastructure.Bus;
+using MediatR;
+
+
+namespace BankingRabbitMQ.Infrastructure.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            //Domain Bus
+            services.AddSingleton<IEventBus, RabbitMQBus>(sp =>
+            {
+                var scopeFactory = sp.GetRequiredService<IServiceScopeFactory>();
+                return new RabbitMQBus(sp.GetService<IMediator>(), scopeFactory);
+            });
+        }
+    }
+}

c98a8067fb240d0f9f731317053b4726ecc3f44d
Rafael Francisco <broncasrafa@gmail.com>
2022-10-22 12:42:19 -0300

    Initial commit for Persistence in Banking Microservice

---
+++ src/BankingRabbitMQ.Service.Banking.Data/Context/BankingDbContext.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using BankingRabbitMQ.Service.Banking.Domain.Models;
+
+namespace BankingRabbitMQ.Service.Banking.Data.Context
+{
+    public class BankingDbContext : DbContext
+    {
+        public BankingDbContext(DbContextOptions<BankingDbContext> options) 
+            : base(options)
+        {
+            ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+        }
+
+        public DbSet<Account> Accounts { get; set; }
+
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.HasDefaultSchema("dbo");
+
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
---
+++ src/BankingRabbitMQ.Service.Banking.Data/Repository/AccountRepository.cs
@@ -0,0 +1,22 @@
+﻿using BankingRabbitMQ.Service.Banking.Data.Context;
+using BankingRabbitMQ.Service.Banking.Domain.Interfaces;
+using BankingRabbitMQ.Service.Banking.Domain.Models;
+
+namespace BankingRabbitMQ.Service.Banking.Data.Repository
+{
+    public class AccountRepository : IAccountRepository
+    {
+        private readonly BankingDbContext _context;
+
+        public AccountRepository(BankingDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public IEnumerable<Account> GetAccounts()
+        {
+            return _context.Accounts;
+        }
+    }
+}
